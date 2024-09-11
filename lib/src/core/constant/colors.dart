7d60da5539f91b33a59aea143754004411dcad65
B1loliddin <biloliddinbotirov000@gmail.com>
2024-09-11 17:47:40 +0500

    added trackers health and translations

--- lib/src/core/constant/colors.dart
+++ lib/src/core/constant/colors.dart
@@ -46,4 +46,6 @@ abstract class AppColors {
 
   static const Color skyBlue = Color(0xFF0D4CEA);
   static const Color deepBlue = Color(0xFF0834A6);
+
+  static Color e8ddf9 = Color(0xFFE8DDF9).withOpacity(0.5);
 }
