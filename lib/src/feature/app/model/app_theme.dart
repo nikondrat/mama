f2f2b42ddd0dd1b07a0e864189c8d426a9e71e25
Polina Yudina <wispy@list.ru>
2024-09-26 22:20:36 +0300

    chat main screen design only

--- lib/src/feature/app/model/app_theme.dart
+++ lib/src/feature/app/model/app_theme.dart
@@ -41,6 +41,11 @@ abstract class _AppThemeStore with Store {
                 fontSize: 17,
                 color: AppColors.greyBrighterColor,
               ),
+              labelMedium: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: AppColors.blackColor,
+              ),
               labelSmall: TextStyle(
                 fontSize: 10,
                 fontWeight: FontWeight.w700,
