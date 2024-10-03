1fd4959a732944eaf4c00a1d6c3c315caa82677c
Nikita Kondratev <nikondratoriginal@gmail.com>
2024-10-03 19:57:27 +0300

    backend

--- lib/src/feature/auth/data/entity/account.dart
+++ lib/src/feature/auth/data/entity/account.dart
@@ -6,9 +6,13 @@ part 'account.g.dart';
 
 @JsonSerializable()
 class AccountModel extends _AccountModel with _$AccountModel {
+  @JsonKey(name: 'fcm_token')
+  String fcmToken;
+
   Gender gender;
 
   AccountModel({
+    this.fcmToken = '',
     required this.gender,
     required super.firstName,
     required super.secondName,
@@ -29,11 +33,14 @@ abstract class _AccountModel with Store {
   });
 
   @observable
+  @JsonKey(name: 'first_name')
   String firstName = '';
 
   @observable
+  @JsonKey(name: 'second_name')
   String secondName = '';
 
   @observable
+  @JsonKey(name: 'phone')
   String phone = '';
 }
