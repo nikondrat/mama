53c1440f3a7ec22e62e2de46c601efb26bfcdf4d
B1loliddin <biloliddinbotirov000@gmail.com>
2024-10-04 11:31:19 +0500

    fix

--- lib/src/feature/home/widgets/body/home_specialist_body.dart
+++ lib/src/feature/home/widgets/body/home_specialist_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mama/src/core/core.dart';
 import 'package:mama/src/feature/home/home.dart';
 
